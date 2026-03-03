dd0eb0fa189bb2b2e8464fe8380ef5ac083b41af
hemerale <89790465+hemerale@users.noreply.github.com>
2026-03-03 22:06:17 +0100

    Update database connection to 'Magazzino'

--- config.php
+++ config.php
@@ -1,9 +1,10 @@
 <?php
 // Configurazione database gestione magazzino
-$conn = new mysqli('localhost', 'root', '', 'LabChimica');
+$conn = new mysqli('localhost', 'root', '', 'Magazzino');
 
 if ($conn->connect_error) {
     die("Connessione fallita: " . $conn->connect_error);
 }
 
 $conn->set_charset("utf8mb4");
+
